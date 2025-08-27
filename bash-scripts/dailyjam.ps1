ecfef13b3563cd14fb45951b7ea438f3cd9cb921
georgewelly <cyberhylian@gmail.com>
2025-08-27 11:33:52 +0100

    added some things

---
+++ bash-scripts/dailyjam.ps1
@@ -0,0 +1,15 @@
+$repoPath = "C:\Users\George Wellington\Documents\workspace\georgewelly.github.io"
+$fileName = "text-files\todaysjam.txt"
+
+Set-Location $repoPath
+
+# Get clipboard text
+$line = Get-Clipboard
+Set-Content $fileName $line
+
+git add $fileName
+git commit -m ("Updated jam text " + (Get-Date -Format "yyyy-MM-dd"))
+git push
+
+
+Pause   # <-- keeps the window open until you press a key
